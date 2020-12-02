2b7211e0ba887e6254a6a352c0d953805045e863
jreyesmercado <47578112+jreyesmercado@users.noreply.github.com>
2020-12-01 20:03:04 -0400

    Create Country

---
+++ JvciOnApp.Web/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using JvciOnApp.Common.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JvciOnApp.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>()
+                .HasIndex(t => t.Name)
+                .IsUnique();
+        }
+    }
+}
